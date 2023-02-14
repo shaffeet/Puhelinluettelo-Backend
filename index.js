e09b88c5fc166f7d5b4cf214f7dfd6c817d9dd6d
Shavit Nahum <shavit.nahum@aalto.fi>
2023-02-14 09:12:54 +0200

    3.8 finished

--- index.js
+++ index.js
@@ -14,8 +14,18 @@ const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: "unknown endpoint" });
 };
 
+morgan.token("postData", (req) => {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body);
+  }
+});
+
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :postData"
+  )
+);
 app.use(requestLogger);
 
 let persons = [
