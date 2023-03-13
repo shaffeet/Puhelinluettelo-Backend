ccf56e070e1842e839fe589abc78b07c781eadaf
Shavit Nahum <shavit.nahum@aalto.fi>
2023-03-13 17:50:44 +0200

    3.11 finished

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 app.use(cors());
+app.use(express.static("build"));
 
 const requestLogger = (req, res, next) => {
   console.log("Method:", req.method);
