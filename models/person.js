d193d6a2d736c5f5165a25d93056c04363d6e2b2
Shavit Nahum <shavit.nahum@aalto.fi>
2023-03-23 13:41:33 +0200

    3.20 finished

--- models/person.js
+++ models/person.js
@@ -21,7 +21,17 @@ const personSchema = new mongoose.Schema({
     minlength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "User phone number required"],
+  },
 });
 
 personSchema.set("toJSON", {
